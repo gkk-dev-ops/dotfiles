3e5d23ef5d3411e1f71752c3a541852d111da6e6
gkk <gkk.dev.ops@gmail.com>
2024-01-10 20:30:06 +0100

    Fixup

--- setup_start.sh
+++ setup_start.sh
@@ -4,6 +4,21 @@
 chsh -s /bin/bash
 
 # setup directory structure
+mkdir "$HOME/dev"
+cd "$HOME/dev"
+
+git clone https://github.com/gkk-dev-ops/dotfiles.git
+
+# Create symlinks to dotfiles in home directory:
+# e.g.
+# ln -s -f ~/dotfiles/.dot_file_name ~/.dot_file_name
+ln -s -f "$HOME/dev/dotfiles/.bash_aliases" $HOME/.bash_aliases
+ln -s -f $HOME/dev/dotfiles/.bashrc $HOME/.bashrc
+ln -s -f $HOME/dev/dotfiles/.gitconfig $HOME/.gitconfig
+ln -s -f $HOME/dev/dotfiles/.profile $HOME/.profile
+ln -s $HOME/dev/dotfiles/bin $HOME/bin
+
+
 mkdir "$HOME/bin"
 
 # install development software
@@ -22,6 +37,18 @@ brew install neofetch
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 # install brew packages
-brew install gcc
-brew install python3
-brew install shellcheck
+if command -v brew --version >/dev/null 2>&1; then
+    brew install gcc
+    brew install python3
+    brew install shellcheck
+else
+    echo "Brew is not installed. Please install Brew first."
+fi
+
+if command -v code --version >/dev/null 2>&1; then
+    chmod +x dotfiles/setup_vscode.sh
+    bash setup_vscode.sh
+else
+    echo "VS Code is not installed. Please install VS Code and ADD TO PATH as \"code\" first."
+fi
+
